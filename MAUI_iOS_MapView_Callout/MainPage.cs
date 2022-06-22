d46f1984f9e77bca30aae3a8df7adaa4918822a4
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-06-22 21:35:49 +0900

    impl to show Pin and Callout

--- MAUI_iOS_MapView_Callout/MainPage.cs
+++ MAUI_iOS_MapView_Callout/MainPage.cs
@@ -1,4 +1,6 @@
-﻿namespace MAUI_iOS_MapView_Callout;
+﻿using Mapsui.UI.Maui;
+
+namespace MAUI_iOS_MapView_Callout;
 
 public partial class MainPage : ContentPage
 {
@@ -7,6 +9,32 @@ public partial class MainPage : ContentPage
 		var mapView = new Mapsui.UI.Maui.MapView();
 		mapView.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
 		Content = mapView;
+
+		Pin pin = new()
+		{
+			Label = "Pin Title",
+			Address = "Pin Subtitle",
+			Type = Mapsui.UI.Maui.PinType.Pin,
+			Position = new(0, 0)
+		};
+
+		Callout callout = pin.Callout;
+
+		callout.Anchor = new(0, pin.Height * pin.Scale);
+		callout.Type = Mapsui.Styles.CalloutType.Detail;
+
+		mapView.PinClicked += (s, e) =>
+		{
+			if (e.Pin is not Pin pin)
+				return;
+
+			if (pin.IsCalloutVisible())
+				pin.HideCallout();
+			else
+				pin.ShowCallout();
+		};
+
+		mapView.Pins.Add(pin);
 	}
 }
 
